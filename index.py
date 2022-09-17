3e1f0ba7473fcb1932beb877abccdd1c6958dd83
dennisbr91 <beltrandennis06@gmail.com>
2022-09-17 19:27:16 -0400

    first

--- index.py
+++ index.py
@@ -140,3 +140,25 @@ if __name__ == '__main__':
     window = Tk()
     application = Product(window)
     window.mainloop()
+
+
+
+-- projects table
+CREATE TABLE IF NOT EXISTS projects (
+	id integer PRIMARY KEY,
+	name text NOT NULL,
+	begin_date text,
+	end_date text
+);
+
+-- tasks table
+CREATE TABLE IF NOT EXISTS tasks (
+	id integer PRIMARY KEY,
+	name text NOT NULL,
+	priority integer,
+	project_id integer NOT NULL,
+	status_id integer NOT NULL,
+	begin_date text NOT NULL,
+	end_date text NOT NULL,
+	FOREIGN KEY (project_id) REFERENCES projects (id)
+);
